e539e36fbd2666d633971be496925099242673ab
orenwf <ofriedman@acm.org>
2018-02-11 13:53:02 -0500

    rearranged interfaces for functions in header file

--- object.c
+++ object.c
@@ -1,15 +1,58 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stddef.h>
 #include <stdbool.h>
-#include <assert.h>
 #include <string.h>
 #include "object.h"
 
+// access function for getting type of object
+const void * type_of(const void * _self) {
+    const struct Object * self = _self;
+    assert(self && self->type);
+    return self->type;
+}
+
+// access function for getting size of object
+size_t size_of(const void * _self) {
+    const struct Type * type = type_of(_self);
+    return type->size;
+}
+
+// access function for getting the parent type of this type
+const void * super(const void * _self) {
+    const struct Type * self = _self;
+    assert(self && self->super);
+    return self->super;
+}
+
+// selector helper function for printfn
+int printfn(const void * self, FILE * fp) {
+    const struct Type * type = type_of(self);
+    assert(type->printfn);
+    return type->printfn(self, fp);
+}
+
+// selector helper function for compare
+int compare(const void * self, const void * other) {
+    const struct Type * type = type_of(self);
+    assert(type->compare);
+    return type->compare(self, other);
+}
+
+// selector helper function for make
+void * ctor(void * _self, va_list * app) {
+    const struct Type * type = type_of(_self);
+    assert(type->ctor);
+    return type->ctor(_self, app);
+}
+
+void * super_ctor(const void * _class, void * _self, va_list * app) {
+    const struct Type * superclass = super(_class);
+    assert(_self && superclass -> ctor);
+    return superclass->ctor(_self, app);
+}
+
 void * make(const void * _type, ...) {
-    const Type * type = _type;
-    Object * object;
+    const struct Type * type = _type;
+    struct Object * object;
     va_list ap;
     assert(type && type->size);
     object = calloc(1, type->size);
@@ -21,11 +64,17 @@ void * make(const void * _type, ...) {
     return object;
 }
 
-// selector helper function for make
-void * ctor(void * _self, va_list * app) {
-    const Type * type = type_of(_self);
-    assert(type->ctor);
-    return type->ctor(_self, app);
+// selector helper function for scrap
+void * dtor(void * _self) {
+    const struct Type * type = type_of(_self);
+    assert(type->dtor);
+    return type->dtor(_self);
+}
+
+void * super_dtor(const void * _class, void * _self) {
+    const struct Type * superclass = super(_class);
+    assert(_self && superclass->dtor);
+    return superclass->dtor(_self);
 }
 
 void scrap(void * _self) {
@@ -33,82 +82,25 @@ void scrap(void * _self) {
         free(dtor(_self));
 }
 
-// selector helper function for scrap
-void * dtor(void * _self) {
-    const Type * type = type_of(_self);
-    assert(type->dtor);
-    return type->dtor(_self);
-}
-
-/*  We obtain instances of a Type by calling make() through the
-    appropriate Type instance member. Type definitions will be
-    statically initialized.
- */
-
-static const Type prototype[] = {
-    {
-            { prototype + 1 },
-            "Object",
-            prototype,
-            sizeof(Object),
-            Object_ctor,
-            Object_dtor,
-            Object_compare,
-            Object_printfn
-    }, {
-            { prototype + 1 },
-            "Type",
-            prototype,
-            sizeof(Type),
-            Type_ctor,
-            Type_dtor,
-            Object_compare,
-            Object_printfn
-    }
-};
-
-const void * Object = prototype;
-const void * Type = prototype+1;
-
 static void * Object_ctor(void * _self, va_list * app) {
-    Object * self = _self;
+    struct Object * self = _self;
     return self;
 }
 
 static void * Object_dtor(void * _self) {
-    Object * self = _self;
+    struct Object * self = _self;
     return self;
 }
 
 static int Object_compare(void * _self, const void * _other) {
-    Object * self = _self, * other = _other;
+    struct Object * self = _self, * other = _other;
     if (self == other) return 0;
     return 1;
 }
 
-// access function for getting type of object
-const void * type_of(const void * _self) {
-    const Object * self = _self;
-    assert(self && self->type);
-    return self->type;
-}
-
-// access function for getting size of object
-size_t size_of(const void * _self) {
-    const Type * type = type_of(_self);
-    return type->size;
-}
-
 static int Object_printfn(const void * _self, FILE * fp) { 
-    const Type * type = type_of(_self);
+    const struct Type * type = type_of(_self);
     return fprintf(fp, "%s at %p\n", type->name, _self);
-}
-
-// access function for getting the parent type of this type
-const void * super(const void * _self) {
-    const Type * self = _self;
-    assert(self && self->super);
-    return self->super;
 }
 
 /*  Type is a subclass of Object (it contains an object) so we inherit 
@@ -117,14 +109,14 @@ const void * super(const void * _self) {
 */
 
 static void * Type_ctor(void * _self, va_list * app) {
-    Type * self = _self;
+    struct Type * self = _self;
     self->name = va_arg(* app, char *);
-    self->super = va_arg(* app, Type *);
+    self->super = va_arg(* app, struct Type *);
     self->size = va_arg(* app, size_t);
     assert(self->super); // important that super is not null !
     /* next we do inheritance from the super class by memcopying from the ctor function
     pointer location through the end of the struct */
-    const size_t offset = offsetof(Type, ctor);
+    const size_t offset = offsetof(struct Type, ctor);
     memcpy( (char *) self + offset, 
             (char *) self->super + offset, 
             size_of(self->super) - offset   );
@@ -132,7 +124,8 @@ static void * Type_ctor(void * _self, va_list * app) {
        the Type constructor argument list.  */
     typedef void (* voidf)(); // generic function pointer
     voidf selector;
-    va_list ap = * app;
+    va_list ap;
+    va_copy(ap,*app);
     
     while ((selector = va_arg(ap, voidf))) {
         voidf method = va_arg(ap, voidf);
@@ -149,10 +142,40 @@ static void * Type_ctor(void * _self, va_list * app) {
 }
 
 static void * Type_dtor(void * _self) {
-    Type * self = _self;
+    struct Type * self = _self;
     fprintf(stderr, "%s: cannot scrap a type definition.\n", self->name);
     return 0;
 }
 // we simply inherit methods from Object type for compare and printfn
 
+
+/*  We obtain instances of a Type by calling make() through the
+    appropriate Type instance member. Type definitions will be
+    statically initialized.
+ */
+
+static const struct Type prototype[] = {
+    {
+            { prototype + 1 },
+            "Object",
+            prototype,
+            sizeof(struct Object),
+            Object_ctor,
+            Object_dtor,
+            Object_compare,
+            Object_printfn
+    }, {
+            { prototype + 1 },
+            "Type",
+            prototype,
+            sizeof(struct Type),
+            Type_ctor,
+            Type_dtor,
+            Object_compare,
+            Object_printfn
+    }
+};
+
+const void * Object = prototype;
+const void * Type = prototype+1;
 
--- object.h
+++ object.h
@@ -1,47 +1,44 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <assert.h>
+
+typedef void * var;
+
 extern const void * Object; /* for make(Object, ...); */
 extern const void * Type; /* for make(Type, "name", parent, size, ..., ); */
 
 void * make(const void * type, ...);
-void * scrap(void * self);
+void scrap(void * self);
 
 int compare(const void * self, const void * other);
 int printfn(const void * self, FILE * fp);
+
+const void * super(const void * _self);
+
+void * super_ctor(const void * _class, void * _self, va_list * app);
+void * super_dtor(const void * _class, void * _self);
 
 /* Type is a summary of the properties and behaviors available to an 
    Object or a struct that inherits from an Object or point to an instance of an 
    Object in their first field. */
 
-typedef struct Object Object;
-typedef struct Type Type;
-
 struct Object {
-    const Type * type;
+    const struct Type * type;
 };
 
 struct Type {
-    const Object _; // type descriptor object
+    const struct Object _; // type descriptor object
     const char * name; // name of the type for printing
-    const Type * super; // parent type of this type
+    const struct Type * super; // parent type of this type
     size_t size;
     void * (* ctor) (void * self, va_list * app);
     void * (* dtor) (void * self);
     int (* compare) (const void * self, const void * other);
     int (* printfn) (const void * self, FILE * fp);
 };
-
-void * super_ctor(const void * _class, void * _self, va_list * app) {
-    const Type * superclass = super(_class);
-    assert(_self && superclass -> ctor);
-    return superclass->ctor(_self, app);
-}
-
-void * super_dtor(const void * _class, void * _self) {
-    const Type * superclass = super(_class);
-    assert(_self && superclass->dtor);
-    return superclass->dtor(_self);
-}
 
 #endif
